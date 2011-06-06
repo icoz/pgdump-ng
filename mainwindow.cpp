74ed13ee1f211651ba40b2d245d2437922e7efb7
icoz <icoz.vt@gmail.com>
2011-06-06 22:50:24 +0400

    added code for test button: testing pgdump.getDatabases

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "pgdump.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -15,5 +16,6 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-
+    Pgdump p;
+    ui->textEdit->setText(p.getDatabases().join("\n"));
 }
