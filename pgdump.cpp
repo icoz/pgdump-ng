f93c6436ffa46a4997deb14ebe5769fb89d870e2
icoz <icoz.vt@gmail.com>
2011-06-06 22:51:12 +0400

    written getDatabases

--- pgdump.cpp
+++ pgdump.cpp
@@ -1,6 +1,38 @@
 #include "pgdump.h"
+#include <QtDebug>
 
-Pgdump::Pgdump(QObject *parent) :
+/*Pgdump::Pgdump(QObject *parent) :
     QObject(parent)
 {
+    db = QSqlDatabase::addDatabase("QPSQL");
+}//*/
+
+Pgdump::Pgdump(QString user,QString pass,QString host,int port,QObject *parent)
+    : QObject(parent)
+{
+    db = QSqlDatabase::addDatabase("QPSQL");
+    db.setHostName(host);
+    db.setPort(port);
+    db.setUserName(user);
+    db.setPassword(pass);
+}
+
+QStringList Pgdump::getDatabases()
+{
+    QStringList sl;
+    db.setDatabaseName("postgres");
+    if (db.open()){
+        qDebug() << "db connected!";
+        QSqlQuery q(db);
+        QString cmd = "SELECT d.datname FROM pg_catalog.pg_database d ORDER BY 1;";
+        if (q.exec(cmd)){
+            qDebug("query runned well!");
+            while (q.next()){
+                sl.append(q.value(0).toString());
+            }
+        }
+        db.close();
+    }
+    qDebug() << sl;
+    return sl;
 }
--- pgdump.h
+++ pgdump.h
@@ -8,15 +8,14 @@ class Pgdump : public QObject
 {
     Q_OBJECT
 public:
-    explicit Pgdump(QObject *parent = 0)
-    {
-        db = QSqlDatabase::addDatabase("QPSQL");
-    }
+    //explicit Pgdump(QObject *parent = 0);
+    Pgdump(QString user="postgres",QString pass="postgres",QString host="localhost",int port=5432,QObject *parent = 0);
     void setHost(QString host="localhost") {db.setHostName(host);}
     void setPort(QString port="5432") {db.setPort(port.toInt());}
     void setPort(int port=5432) {db.setPort(port);}
     void setUser(QString user="user") {db.setUserName(user);}
-    void setPassword(QString user="pass") {db.setPassword(pass);}
+    void setPassword(QString pass="pass") {db.setPassword(pass);}
+    bool tryOpen() {return db.open();}
     QStringList getDatabases();
 signals:
 
